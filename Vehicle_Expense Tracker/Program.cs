ce8379e77fb65c64271790901279835dd5cf0b5d
viswa-developer <richavish2003@gmail.com>
2026-01-31 10:56:54 +0530

    dotnet8

--- Vehicle_Expense Tracker/Program.cs
+++ Vehicle_Expense Tracker/Program.cs
@@ -22,15 +22,15 @@ builder.Services.AddDBService();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();  // Generates OpenAPI JSON
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();     // Serves JSON at /swagger/v1/swagger.json
+    app.UseSwaggerUI();   // Serves UI at /swagger
 }
-
 //app.UseHttpsRedirection();
 
 app.UseAuthorization();
